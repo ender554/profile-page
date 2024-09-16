8ccf3fd6fd4e11669d65d5c4e0dd5a412a0f4af3
ender554 <joshadams554@gmail.com>
2024-09-16 10:59:45 -0700

    remove ref to web vitals

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
@@ -10,6 +9,6 @@ if (rootElement) {
   root.render(
     <React.StrictMode>
       <App />
-    </React.StrictMode>
+    </React.StrictMode>,
   );
 }
